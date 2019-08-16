574d66d22a2e683838d1d5519e1f3a830f626faa
ph4nt0m_gh0sT <ph4nt0mgh0sT17@gmail.com>
2019-08-16 12:14:58 +0200

    Added chapter 1.

--- Source/EFCore/EFCoreChapter1/Program.cs
+++ Source/EFCore/EFCoreChapter1/Program.cs
@@ -6,7 +6,7 @@ namespace EFCoreChapter1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Chapter 1!");
         }
     }
 }
